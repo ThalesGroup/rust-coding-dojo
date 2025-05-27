7a58e93b11a1b340c04ad570a3d175fce21c1935
0xPraedico <meowmeowcapital@gmail.com>
2025-05-27 22:12:12 +0200

    update 02-structure

--- katas/02-structure/00-basics/solutions/introduction.rs
+++ katas/02-structure/00-basics/solutions/introduction.rs
@@ -1,22 +1,33 @@
 /**
  * Section 1 : Introduction
- * - *Exercise 1:* Define an Enum BookType with some kinds like `comic`, `sf` or `novel`.
+ * - *Exercise 1:* Define an Enum BookType with some kinds like `comic`, `sf`, `novel` and `programming`.
  * - *Exercise 2:* Define a struct `Book` with fields for title, author, book type and publication year. Instantiate an instance and print its details
  * - *Exercise 3:* Create a struct `Library` containing a vector of `Book` instances. Write a function to display all books in the library.
  * - *Exercise 4:* Extend the `Book` struct with a method to calculate the age of the book based on the current year. Display the age of each book in the library.
  *
+ * Tip: Derive `Debug` so we can print `BookType` with `{:?}`
  * Define in this module the types, methods and function and use them in `main.rs`.
  * The exercises in this coding dojo are designed to progressively increase in difficulty.
  */
 
 // Section 1: Introduction - Exercise 1
+#[derive(Debug)]
+pub enum BookType {
+    Comic,
+    SF,
+    Novel,
+    Programming,
+}
+
+// Section 1: Introduction - Exercise 2
 pub struct Book {
     pub title: String,
     pub author: String,
     pub publication_year: u32,
+    pub book_type: BookType
 }
 
-// Section 1: Introduction - Exercise 2
+// Section 1: Introduction - Exercise 3
 pub struct Library {
     pub books: Vec<Book>,
 }
@@ -24,12 +35,12 @@ pub struct Library {
 impl Library {
     pub fn display_books(&self) {
         for book in &self.books {
-            println!("Title: {}, Author: {}, Year: {}", book.title, book.author, book.publication_year);
+            println!("Title: {}, Author: {}, Year: {}, Type: {:?}", book.title, book.author, book.publication_year, book.book_type);
         }
     }
 }
 
-// Section 1: Introduction - Exercise 3
+// Section 1: Introduction - Exercise 4
 impl Book {
     pub fn calculate_age(&self, current_year: u32) -> u32 {
         current_year - self.publication_year
--- katas/02-structure/00-basics/src/introduction.rs
+++ katas/02-structure/00-basics/src/introduction.rs
@@ -1,10 +1,11 @@
 /**
  * Section 1 : Introduction
- * - *Exercise 1:* Define an Enum BookType with some kinds like `comic`, `sf` or `novel`.
+ * - *Exercise 1:* Define an Enum BookType with some kinds like `comic`, `sf`, `novel` and `programming`.
  * - *Exercise 2:* Define a struct `Book` with fields for title, author, book type and publication year. Instantiate an instance and print its details
  * - *Exercise 3:* Create a struct `Library` containing a vector of `Book` instances. Write a function to display all books in the library.
  * - *Exercise 4:* Extend the `Book` struct with a method to calculate the age of the book based on the current year. Display the age of each book in the library.
  *
+ * Tip: Derive `Debug` so we can print `BookType` with `{:?}`
  * Define in this module the types, methods and function and use them in `main.rs`.
  * The exercises in this coding dojo are designed to progressively increase in difficulty.
  */
